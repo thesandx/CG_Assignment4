482f266a8db5a0cb5a881cf4b6bc686745d2d87d
thesandx <kumarjha.sandeep@gmail.com>
2021-04-16 02:44:54 +0530

    Initial ThreeJS Setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 const bs = require('browser-sync').create(); // create a browser sync instance.
 
-/* Run a server in source folder and refresh server and browser if any file inside it changes */
+/* Run a server in source folder and refresh server and browser if any file inside it changes  */
 gulp.task('browser-sync', function ()
 {
 	bs.init({
